f281543642cf34f0815413d2f707e85b7c31a0dc
Cyrille Lora Ronco <79576141+cloraronco@users.noreply.github.com>
2023-11-02 09:53:43 +0100

    Add files via upload

---
+++ display_image.py
@@ -0,0 +1,9 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+from zipfile import ZipFile
+from urllib.request import urlretrieve
+
+from IPython.display import Image
